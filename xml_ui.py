003daa449800093ab0bbf27c69aa7c31ecacb9eb
grantbowlds-nick <162386286+grantbowlds-nick@users.noreply.github.com>
2024-06-03 19:41:15 -0700

    I think I got the notes thing working

--- xml_ui.py
+++ xml_ui.py
@@ -7,6 +7,7 @@ from xml.etree.ElementTree import XML
 from xml_helpers.ingest import *
 from xml_helpers.reports import *
 from xml_helpers.filter import *
+from xml_helpers.syncsketch import *
 
 CURRENT_EPISODE = None
 XML_FUNCTIONS = []
@@ -15,7 +16,7 @@ XML_FUNCTIONS = []
 def create_button(label, function):
     global XML_FUNCTIONS
     button = ttk.Button(frm, text=label, command=function)
-    button.grid(column=len(XML_FUNCTIONS), row=1)
+    button.grid(column=len(XML_FUNCTIONS), row=2)
     button.config(state=DISABLED)
     XML_FUNCTIONS.append(button)
 
@@ -63,6 +64,10 @@ def output_filtered_xml():
     show_output(write_filtered(CURRENT_EPISODE, xml_file_string.get()))
 
 
+def output_syncsketch():
+    show_output(upload(CURRENT_EPISODE, ss_link.get()))
+
+
 root = tk.Tk()
 root.resizable(True, True)
 root.title("MATM -- Upload a CSV to SyncSketch")
@@ -72,18 +77,25 @@ frm.grid()
 xml_file_string = tk.StringVar()
 xml_file_string.set("Please choose an XML file")
 
+ss_link = tk.StringVar()
+ss_link.set("If using upload, enter a syncsketch link")
 
 create_button("Audio report", output_audio)
 create_button("CG Fixes report", output_cgfixes)
 create_button("Conform report", output_conform)
 create_button("Output filtered XML", output_filtered_xml)
+create_button("Upload notes to syncsketch", output_syncsketch)
 
-ss_textbox = tk.Entry(frm, textvariable=xml_file_string, width=100).grid(
+xml_textbox = tk.Entry(frm, textvariable=xml_file_string, width=100).grid(
     column=0, row=0, sticky="news", columnspan=len(XML_FUNCTIONS) - 1
 )
 
 ttk.Button(frm, text="Choose an xml", command=xml_to_episode).grid(
     column=len(XML_FUNCTIONS) - 1, row=0
+)
+
+syncsketch_textbox = tk.Entry(frm, textvariable=ss_link, width=100).grid(
+    column=0, row=1, sticky="news", columnspan=len(XML_FUNCTIONS) - 1
 )
 
 root.mainloop()
